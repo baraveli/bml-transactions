1ab16015b0ddbf82353cefea0bd352abffc03d79
Mohamed Jinas <j@live.mv>
2020-12-07 19:25:27 +0500

    Added tests

---
+++ tests/BMLTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use Baraveli\BMLTransaction\BML;
+
+class BMLTest extends \PHPUnit\Framework\TestCase
+{
+    protected $bml;
+
+    protected function setUp(): void
+    {
+        $bml = new BML();
+        $this->bml = $bml->login($_ENV['BML_USERNAME'], $_ENV['BML_PASSWORD']);
+    }
+
+    /** @test */
+    public function can_login_and_authenticate_with_bml()
+    {
+        $bml = new BML();
+
+        $response = $bml->login($_ENV['BML_USERNAME'], $_ENV['BML_PASSWORD']);
+        $this->assertEquals(true, $response->authenticationStatus);
+    }
+
+    /** @test */
+    public function it_can_get_transactions_made_today()
+    {
+        $response = $this->bml->GetTodayTransactions();
+
+        $this->assertNotEmpty($response);
+    }
+
+    /** @test */
+    public function it_can_get_pending_transactions()
+    {
+        $response = $this->bml->GetPendingTransactions();
+
+        $this->assertNotEmpty($response);
+    }
+
+    /** @test */
+    public function it_can_get_the_transactions_made_between_certain_dates()
+    {
+        $start = "December 15 2019";
+        $end = "August 1 2020";
+        $response = $this->bml->GetTransactionsBetween($start, $end);
+
+        $date = strtotime($response["history"][0]["bookingDate"]);
+
+        $this->assertTrue($date >= strtotime($start) && $date <= strtotime($end));
+    }
+}
---
+++ tests/ClientTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Baraveli\BMLTransaction\Client;
+use Baraveli\BMLTransaction\BML;
+
+class ClientTest extends \PHPUnit\Framework\TestCase
+{
+    protected $client;
+
+
+    protected function setUp(): void
+    {
+        $this->client = new Client();
+        $this->client->PostRequest(['j_username' => $_ENV['BML_USERNAME'], 'j_password' =>  $_ENV['BML_PASSWORD']], 'm/login');
+    }
+
+    /** @test */
+    public function can_send_get_request()
+    {
+
+        $response = $this->client->GetRequest("profile");
+        $this->assertIsArray($response);
+    }
+    /** @test */
+    public function can_send_post_request()
+    {
+        $response = $this->client->PostRequest(['j_username' => $_ENV['BML_USERNAME'], 'j_password' =>  $_ENV['BML_PASSWORD']], 'm/login');
+        $this->assertIsArray($response);
+        $this->assertEquals(true, $response["authenticated"]);
+    }
+}
